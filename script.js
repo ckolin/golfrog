9f5609c4c9e658f95876881cc142ad3bf79a4f2d
ckolin <25738463+ckolin@users.noreply.github.com>
2024-08-15 19:13:03 +0200

    WIP Implement jumping

--- script.js
+++ script.js
@@ -11,12 +11,20 @@ const player = {
     vel: Vec.zero(),
     damping: 0.3,
     gravity: 1,
-    bounce: 1,
+    bounce: 0.5,
+    grounded: false,
+    jump: 10,
 };
 
 const entities = [player];
 
 const ground = (x) => 0.8 - 0.05 * Math.sin(10 * x) + 0.05 * Math.sin(2 * x);
+
+const input = {
+    primary: false,
+    dragStart: Vec.zero(),
+    dragEnd: Vec.zero(),
+};
 
 const draw = () => {
     update();
@@ -33,6 +41,11 @@ const draw = () => {
         ctx.save();
         ctx.lineWidth = 0.01;
         ctx.globalAlpha = 0.3;
+        // Drag
+        ctx.beginPath();
+        ctx.moveTo(input.dragStart.x, input.dragStart.y);
+        ctx.lineTo(input.dragEnd.x, input.dragEnd.y);
+        ctx.stroke();
         // Ground
         ctx.beginPath();
         const steps = 100;
@@ -68,8 +81,8 @@ const draw = () => {
             pre.id = "dbg";
             pre.style.position = "absolute";
             pre.style.pointerEvents = "none";
-            pre.style.color = "#fff";
-            pre.style.background = "#0008";
+            pre.style.color = "white";
+            pre.style.textShadow = "1px 1px black";
             document.body.prepend(pre);
         }
         pre.innerText = JSON.stringify(dbg, (k, v) => v.toFixed == null ? v : Number(v.toFixed(3)), 2);
@@ -86,8 +99,18 @@ const update = () => {
     const dt = (now - last) / 1000;
     last = now;
 
+    // Detect drag
+    if (player.grounded && !input.primary) {
+        const drag = Vec.subtract(input.dragEnd, input.dragStart);
+        if (Vec.length(drag) > 0.02) {
+            player.vel = Vec.add(player.vel, Vec.scale(drag, -player.jump));
+            player.grounded = false;
+        }
+        input.dragStart = input.dragEnd = Vec.zero();
+    }
+
     // Gravity
-    for (const e of entities.filter(e => e.gravity)) {
+    for (const e of entities.filter(e => e.gravity && !e.grounded)) {
         e.vel = Vec.add(e.vel, { x: 0, y: e.gravity * dt });
     }
     // Velocity
@@ -99,15 +122,39 @@ const update = () => {
         e.vel = Vec.scale(e.vel, Math.pow(e.damping, dt));
     }
     // Bounce off ground
-    for (const e of entities.filter(e => e.bounce)) {
-        if (e.pos.y > ground(e.pos.x)) {
+    for (const e of entities.filter(e => e.bounce && !e.grounded)) {
+        const elev = e.pos.y - ground(e.pos.x);
+        if (elev < 0) {
             e.pos.y = ground(e.pos.x);
             e.vel.y *= -e.bounce;
+        }
+        if (Vec.length(e.vel) < 1e-2) {
+            e.grounded = true;
         }
     }
 
     dbg.dt = dt;
     dbg.player = player;
+    dbg.input = input;
 };
+
+const getRelativeCoords = (e) => ({ x: e.offsetX / canvas.width, y: e.offsetY / canvas.height });
+
+canvas.addEventListener("mousedown", (e) => {
+    input.primary = true;
+    input.dragStart = input.dragEnd = getRelativeCoords(e);
+});
+canvas.addEventListener("mousemove", (e) => {
+    if (input.primary) {
+        input.dragEnd = getRelativeCoords(e);
+    }
+});
+canvas.addEventListener("mouseup", (e) => {
+    input.primary = false;
+});
+canvas.addEventListener("mouseleave", () => {
+    input.primary = false;
+    input.dragStart = input.dragEnd;
+});
 
 draw();
